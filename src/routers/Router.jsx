65748c02a56a4194b97983bbe9cf029e6232a2bd
junaid014 <junaidhossen014@gmail.com>
2025-06-08 22:07:38 +0600

    initial project setup with tailwind daisy react router

---
+++ src/routers/Router.jsx
@@ -0,0 +1,25 @@
+import {
+  createBrowserRouter,
+ 
+} from "react-router";
+import Root from "../Layouts/Root";
+import Home from "../Pages/Home";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+
+    children:[
+        {
+            path:'/',
+            index:true,
+            Component:Home
+        }
+    ]
+
+   
+  },
+]);
