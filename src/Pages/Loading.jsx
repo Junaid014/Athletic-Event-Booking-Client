3723df7da9003770d7f8793689d9c9acf84c369a
junaid014 <junaidhossen014@gmail.com>
2025-07-27 12:20:58 +0600

    add loading component

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className='flex min-h-screen justify-center items-center'>
-            <span className='loading loading-bars loading-xl'></span>
+        <div className="loading-container">
+            <div className="sk-folding-cube">
+                <div className="sk-cube1 sk-cube"></div>
+                <div className="sk-cube2 sk-cube"></div>
+                <div className="sk-cube4 sk-cube"></div>
+                <div className="sk-cube3 sk-cube"></div>
+            </div>
         </div>
     );
 };
 
 export default Loading;
+
